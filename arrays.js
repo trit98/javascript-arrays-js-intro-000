2aa01b447a7bdd4415cc1e054542800c75c15263
Talia Ritholtz <talia.ritholtz@baruchmail.cuny.edu>
2019-01-20 15:35:02 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,25 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+const addElementToBeginningOfArray = ['foo', ...array];
+return addElementToBeginningOfArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+const destructivelyAddElementToBeginningOfArray = ['foo', ...array];
+return destructivelyAddElementToBeginningOfArray;
+}
+
+function addElementToEndOfArray(array, element) {
+const addElementToEndOfArray = [...array, 'foo'];
+return addElementToEndOfArray;
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  const destructivelyAddElementToEndOfArray = [...array, 'foo'];
+  return destructivelyAddElementToEndOfArray;
+}
+
+function accessElementInArray(array, index) {
+  console.log(accessElementInArray[1]);
+}
