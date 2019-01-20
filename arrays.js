f8b112804140a64478e5007c85e359f06dc4f57f
Talia Ritholtz <talia.ritholtz@baruchmail.cuny.edu>
2019-01-20 16:09:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element, ...array)
-  return array
+  return [element, ...array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
